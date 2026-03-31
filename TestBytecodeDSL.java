b99f78ee553340e73e17a3f9e9b05af4481fcbc0
Karl Pietrzak <karl@medplum.com>
2026-03-31 08:18:40 -0400

    dead code

--- TestBytecodeDSL.java
+++
@@ -1,7 +0,0 @@
-import com.oracle.truffle.api.bytecode.GenerateBytecode;
-
-public class TestBytecodeDSL {
-    public static void main(String[] args) {
-        System.out.println("Available!");
-    }
-}
